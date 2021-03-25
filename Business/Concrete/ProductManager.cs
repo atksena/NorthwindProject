d0200cf120147119201c424ae031452f211f022e
SENA ATAK <ataksena98@gmail.com>
2021-03-25 17:41:32 +0300

    .

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -17,15 +19,32 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
-        public List<Product> GetAll()
+
+        public IResult Add(Product product)
+        {
+            if (product.ProductName.Length<2)
+            {
+                //magic string : string ifadelerin ayrı ayrı yazılması
+                return new ErrorResult(Messages.ProductNameInvalid);
+            }
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
+        }
+
+        public IDataResult<List<Product>> GetAll()
         {
             //İş kodları
-            return _productDal.GetAll();
+            return  new DataResult<List<Product>>(_productDal.GetAll(),true,"Ürün Listelendi");
         }
 
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p=>p.ProductId==productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
@@ -37,5 +56,6 @@ namespace Business.Concrete
         {
             return _productDal.GetProductDetails();
         }
+
     }
 }
---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    //T hangi tipi döndüreceğini söyler 
+    public interface IDataResult<T> : IResult
+    {
+        //İçinde mesaj ve işlem sonucu haricinde T türünde bir data olmalı
+        //T yapmamızın sebebi ürün dödürebilir kategory veya mişteriler tablosu gibi..
+        T Data { get; }
+    }
+}
+ 
